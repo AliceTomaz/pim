3a8e3032ff9eb610e6d1a2990163974c9163ed35
AliceTomaz <Alytomaz4@gmail.com>
2024-11-20 16:46:46 -0300

    codigo atualizado

--- caixa.h
+++ caixa.h
@@ -2,74 +2,87 @@
 #define CAIXA_H
 #include "hortiFrutiLib.h"
 
-#define MAX_TENTATIVAS 5
-#define LOGIN_CAIXA "HORTI05"
-#define SENHA_CAIXA "F12345"
+/* 
+ * Definicoes e constantes 
+ */
+#define MAX_TENTATIVAS 5          // Numero maximo de tentativas para login do caixa
+#define LOGIN_CAIXA "HORTI05"     // Login padrao do caixa
+#define SENHA_CAIXA "F12345"      // Senha padrao do caixa
 
+/* 
+ * Funcao para autenticacao do caixa e gerenciamento de operacoes relacionadas 
+ */
 void senhaCaixa() {
-    limparTela();
-    char login[20];
-    char senha[20];
-    int tentativas = 0;
+    limparTela(); // Limpa a tela antes de iniciar a autenticacao
+    char login[20];  // Armazena o login digitado pelo usuario
+    char senha[20];  // Armazena a senha digitada pelo usuario
+    int tentativas = 0; // Contador de tentativas de login
 
+    /* Loop para verificar as credenciais do caixa */
     while (tentativas < MAX_TENTATIVAS) {
         printf("Login: ");
-        scanf("%s", login);
+        scanf("%s", login); // Captura o login
         printf("Senha: ");
-        scanf("%s", senha);
-        limparTela();
+        scanf("%s", senha); // Captura a senha
+        limparTela(); // Limpa a tela apos a entrada de dados
 
+        /* Verifica se o login e a senha correspondem aos valores definidos */
         if (strcmp(login, LOGIN_CAIXA) == 0 && strcmp(senha, SENHA_CAIXA) == 0) {
             printf("Bem vindo, HORTI05! \n\n");
 
-            int opcao;
+            int opcao; // Variavel para armazenar a escolha no menu
 
-            carregarProdutos();
+            carregarProdutos(); // Carrega a lista de produtos do estoque
 
+            /* Exibe o cabecalho inicial do menu */
             printf("----------------------------\n");
             printf("-------- HORTIFRUTI --------\n");
             printf("----------------------------\n");
 
+            /* Menu de operacoes do caixa */
             do {
                 printf("1 - Editar Produto      \n");
                 printf("2 - Visualizar Produto  \n");
                 printf("3 - Visualizar Estoque  \n");
                 printf("4 - Compra (Cliente)    \n");
                 printf("5 - Sair do Programa    \n");
-                scanf("%d", &opcao);
-                getchar();
+                scanf("%d", &opcao); // Captura a opcao do caixa
+                getchar(); // Consome o caractere de nova linha para evitar erros
 
+                /* Processa a opcao escolhida pelo caixa */
                 switch (opcao) {
                     case 1:
-                        editarProduto();
+                        editarProduto(); // Permite editar um produto
                         break;
                     case 2:
-                        visualizarProduto();
+                        visualizarProduto(); // Exibe detalhes de um produto especifico
                         break;
                     case 3:
-                        visualizarTodosProdutos();
+                        visualizarTodosProdutos(); // Exibe o estoque completo
                         break;
                     case 4:
-                        realizarCompra();
+                        realizarCompra(); // Inicia o processo de compra para um cliente
                         break;
                     case 5:
-                        sair();
+                        sair(); // Sai do programa
                         break;
                     default:
-                        limparTela();
+                        limparTela(); // Limpa a tela e informa erro em caso de opcao invalida
                         printf("Opcao invalida.\n");
                         break;
                 }
 
-            } while (opcao != 5);
+            } while (opcao != 5); // Mantem o menu ativo ate o caixa optar por sair
         } else {
+            /* Caso as credenciais sejam invalidas */
             printf("Usuario ou senha invalido.\n");
-            tentativas++;
+            tentativas++; // Incrementa o numero de tentativas
             printf("Tentativas restantes: %d\n", MAX_TENTATIVAS - tentativas);
         }
     }
 
-    acessoNegado();
+    /* Acao tomada apos exceder o numero maximo de tentativas */
+    acessoNegado(); // Funcao que lida com a rejeicao do acesso
 }
 
 #endif
--- gerente.h
+++ gerente.h
@@ -2,34 +2,41 @@
 #define GERENTE_H
 #include "hortiFrutiLib.h"
 
-#define MAX_TENTATIVAS 5
-#define LOGIN_GERENTE "HORTI10"
-#define SENHA_GERENTE "G12345"
 
+#define MAX_TENTATIVAS 5          // Numero maximo de tentativas para login do gerente
+#define LOGIN_GERENTE "HORTI10"   // Login  do gerente
+#define SENHA_GERENTE "G12345"    // Senha  do gerente
+
+/* 
+ * Funçao para autenticaçao do gerente e gerenciamento de operações relacionadas 
+ */
 void senhaGerente() {
-    limparTela();
-    char login[20];
-    char senha[20];
-    int tentativas = 0;
+    limparTela(); // Limpa a tela antes de iniciar a autenticação
+    char login[20];  // Armazena o login digitado pelo usuário
+    char senha[20];  // Armazena a senha digitada pelo usuário
+    int tentativas = 0; // Contador de tentativas de login
 
+    /* Loop para verificar as credenciais do gerente */
     while (tentativas < MAX_TENTATIVAS) {
         printf("Login: ");
-        scanf("%s", login);
+        scanf("%s", login); //  login
         printf("Senha: ");
-        scanf("%s", senha);
-        limparTela();
+        scanf("%s", senha); // senha
+        limparTela(); // Limpa a tela após a entrada de dados
 
+        /* Verifica se o login e a senha correspondem aos valores definidos */
         if (strcmp(login, LOGIN_GERENTE) == 0 && strcmp(senha, SENHA_GERENTE) == 0) {
             printf("Bem vindo, HORTI10! \n\n");
 
-            int opcao;
+            int opcao; // Variável para armazenar a escolha no menu
 
-            carregarProdutos();
+            carregarProdutos(); // Carrega a lista de produtos do estoque
 
             printf("----------------------------\n");
             printf("-------- HORTIFRUTI --------\n");
             printf("----------------------------\n");
 
+            /* Menu de operaçoes do gerente */
             do {
                 printf("1 - Cadastrar Produto   \n");
                 printf("2 - Visualizar Estoque  \n");
@@ -37,43 +44,46 @@ void senhaGerente() {
                 printf("4 - Visualizar Produto  \n");
                 printf("5 - Excluir produto     \n");
                 printf("6 - Sair do Programa    \n");
-                scanf("%d", &opcao);
-                getchar();
+                scanf("%d", &opcao); // Captura a opção do gerente
+                getchar(); // Consome o caractere de nova linha para evitar erros
 
+                /* Processa a opção escolhida pelo gerente */
                 switch (opcao) {
                     case 1:
-                        cadastrarProduto();
+                        cadastrarProduto(); // Cadastra um novo produto
                         break;
                     case 2:
-                        visualizarTodosProdutos();
+                        visualizarTodosProdutos(); // Exibe o estoque completo
                         break;
                     case 3:
-                        editarProduto();
+                        editarProduto(); // Permite editar um produto
                         break;
                     case 4:
-                        visualizarProduto();
+                        visualizarProduto(); // Exibe detalhes de um produto específico
                         break;
                     case 5:
-                        excluirProduto();
+                        excluirProduto(); // Exclui um produto do estoque
                         break;
                     case 6:
-                        sair();
+                        sair(); // Sai do programa
                         break;
                     default:
-                        limparTela();
+                        limparTela(); // Limpa a tela e informa erro em caso de opção inválida
                         printf("Opcao invalida.\n");
                         break;
                 }
 
-            } while (opcao != 6);
+            } while (opcao != 6); // Mantem o menu ativo ate o gerente optar por sair
         } else {
+            /* Caso as credenciais sejam invalidas */
             printf("Usuario ou senha invalido.\n");
-            tentativas++;
+            tentativas++; // Incrementa o numero de tentativas
             printf("Tentativas restantes: %d\n", MAX_TENTATIVAS - tentativas);
         }
     }
 
-    acessoNegado();
+    /* Açao tomada apos exceder o numero maximo de tentativas */
+    acessoNegado(); // Funçao que lida com a rejeiçao do acesso
 }
 
 #endif
